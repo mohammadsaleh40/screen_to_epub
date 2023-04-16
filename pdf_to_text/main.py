832082c9792812665aa8664086e0944b7dc8dfb4
mohammadsaleh40 <mohammadsalehmohammadsaleh@gmail.com>
2023-04-16 09:49:35 +0330

    اضافه کردن به لیست tasvir و ignor

--- pdf_to_text/main.py
+++ pdf_to_text/main.py
@@ -8,6 +8,46 @@ from pytesseract import Output
 dirname = os.path.dirname(__file__)
 dirname
 # %%
+def update_img():
+    global asli , pardazesh
+    img = asli.copy()
+    for i in range(0,len(ignore_list)//2,2):
+        cv.rectangle(img, ignore_list[i], ignore_list[i+1], (255, 255, 255), -1)
+    
+    for i in range(0,len(tasvir_list)//2,2):
+        cv.rectangle(img, tasvir_list[i], tasvir_list[i+1], (255, 255, 255), -1)
+    pardazesh = img
+
+# %%
+ignore_list = []
+tasvir_list = []
+def mouse_click(event, x, y, flags, param):
+    global ignore_list , tasvir_list
+    # add
+    if event == cv.EVENT_LBUTTONDOWN and param == 'i':
+        ignore_list.append([int(x * (1/scale)) , int(y* (1/scale))])
+    elif event == cv.EVENT_LBUTTONUP and param == 'i':
+        ignore_list.append([int(x * (1/scale)) , int(y* (1/scale))])
+    # remove
+    if event == cv.EVENT_RBUTTONDOWN and param == 'i':
+        ignore_list.pop()
+    elif event == cv.EVENT_RBUTTONUP and param == 'i':
+        ignore_list.pop()
+    # add
+    if event == cv.EVENT_LBUTTONDOWN and param == 't':
+        tasvir_list.append([int(x * (1/scale)) , int(y* (1/scale))])
+    elif event == cv.EVENT_LBUTTONUP and param == 't':
+        tasvir_list.append([int(x * (1/scale)) , int(y* (1/scale))])
+    # remove
+    if event == cv.EVENT_RBUTTONDOWN and param == 't':
+        tasvir_list.pop()
+    elif event == cv.EVENT_RBUTTONUP and param == 't':
+        tasvir_list.pop()
+    
+    
+    if event == cv.EVENT_LBUTTONDOWN and param == 'space':
+        update_img()
+# %%
 books_path = dirname[:-11]+"books/"
 book_name = "02024226sanjabbook.ir.pdf"
 # %%
@@ -18,7 +58,8 @@ for index , img in enumerate(images):
 img_data = pytesseract.image_to_data(images[0], lang='fas+eng' , output_type=Output.DATAFRAME)
 img_data
 # %%
-gg = cv.cvtColor(np.array(images[0]) , cv.COLOR_GRAY2BGR)
+asli = np.array(images[0])
+pardazesh = cv.cvtColor(asli , cv.COLOR_GRAY2BGR)
 for i in range(len(img_data["level"])):
     if img_data["level"][i] == 5:
         index = i
@@ -28,27 +69,37 @@ for i in range(len(img_data["level"])):
         x2 = x1 + int(img_data['width'][index])
         y2 = y1 + int(img_data['height'][index])
         zarib_sehat = img_data["conf"][index]
-        cv.rectangle(gg ,  (x1, y1), (x2, y2), ((zarib_sehat*255)/100,(zarib_sehat*70)/100,(zarib_sehat*70)/100), 10)
-        """
-        if img_data["conf"][i] > 80:
-            cv.rectangle(gg ,  (x1, y1), (x2, y2), (127,170,60), 2)
-        elif img_data["conf"][i] > 50:
-            cv.rectangle(gg ,  (x1, y1), (x2, y2), (224, 0 , 0), 2)
-        elif img_data["conf"][i] > 20:
-            cv.rectangle(gg ,  (x1, y1), (x2, y2), (0, 0 , 224), 2)
-        else:
-            cv.rectangle(gg ,  (x1, y1), (x2, y2), (0, 225 , 0), 2)
-        """
-
-
+        cv.rectangle(pardazesh ,  (x1, y1), (x2, y2), ((zarib_sehat*255)/100,(zarib_sehat*70)/100,(zarib_sehat*70)/100), 10)
+t = ""
+scale = 0.2
 while True:
-    cv.imshow("salam", cv.resize(gg , (0,0),None , 0.2 , 0.2))
+    
+    namayesh= pardazesh.copy()
+    
+    cv.putText(img = namayesh, text = t , org= (40 , 100),
+        fontFace=cv.FONT_HERSHEY_TRIPLEX, fontScale=3,
+        color=(0, 255, 0),thickness=3)
+        
+    cv.imshow("salam", cv.resize(namayesh, (0,0),None , scale , scale))
+    
     key = cv.waitKey(5)
     if key == ord('q'):
         cv.destroyAllWindows()
         break
-
-# %%
+    elif key == ord('i'):
+        t = "i"
+        cv.setMouseCallback('salam', mouse_click , 'i')
+    elif key == ord('t'):
+        cv.setMouseCallback('salam', mouse_click , 't')
+        t = "t"
+    elif key == 32:
+        t = ""
+        cv.setMouseCallback('salam', mouse_click , 'space')
+ # %%
 # %%
 img_data.head(20)
+# %%
+tasvir_list
+# %%
+ignore_list
 # %%
